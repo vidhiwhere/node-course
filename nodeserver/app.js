87606b1fb81df4e84cf34e7070ad60e7ad553b59
vidhiwhere <vidhikumari6025@gmail.com>
2026-04-24 19:55:26 +0530

    refactor server listening logic to use a constant for the port

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -5,5 +5,7 @@ const http=require('http');
 const server=http.createServer((req, res)=>{
   console.log(req);
 });
-
-server.listen(3000);
+const PORT=3000;
+server.listen(PORT, ()=>{
+  console.log('server running at port 3000');
+});
