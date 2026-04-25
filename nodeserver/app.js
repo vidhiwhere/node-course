cf4bb098ceb749105b2a54f88423631af22dabf6
vidhiwhere <vidhikumari6025@gmail.com>
2026-04-25 11:46:28 +0530

    enhance request logging to include URL, method, and headers

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -3,7 +3,8 @@ const http=require('http');
 
 
 const server=http.createServer((req, res)=>{
-  console.log(req);
+  console.log(req.url , req.method, req.headers);
+ 
 });
 const PORT=3000;
 server.listen(PORT, ()=>{
